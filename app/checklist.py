26ccf8b0054f946560d38b447d55e201ee623e95
Jean Petitclerc <jean.petitclerc@groupepp.com>
2017-02-14 06:19:09 -0500

    First form.

--- app/checklist.py
+++ app/checklist.py
@@ -1,14 +1,36 @@
 from flask import Flask, request, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY']  = 'NotSoSecretKey'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/user/<name>')
 def user(name):
